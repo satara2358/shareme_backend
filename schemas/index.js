60a80d7b83d43c172257d1302526d572ad8504e0
satara <25549072+satara2358@users.noreply.github.com>
2023-07-25 22:33:21 -0500

    Fronted_init

--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,12 @@
+// import createSchema from 'part:@sanity/base/schema-creator'
+// import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+import comment from './comment';
+import pin from './pin';
+import postedBy from './postedBy';
+import save from './save';
 import user from './user';
 
 export const schemaTypes = [
-  user
+  user, pin, postedBy, save, comment
 ]
